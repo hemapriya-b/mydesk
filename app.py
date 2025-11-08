b2d64cac59406005eeb381069a40f2aa502bbf50
hemapriya-b <writetohemapriyab@gmail.com>
2025-11-08 20:14:43 +0530

    Fix deployment issue and update code

--- app.py
+++ app.py
@@ -406,5 +406,7 @@ def search():
                          subjects_results=subjects_results,
                          units_results=units_results)
 
-# with app.app_context():
-#    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
